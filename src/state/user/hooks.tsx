da32e42b7f330c15ee7b55a217f83afe4efe8da3
pippiadmin <heco.punk0@gmail.com>
2021-08-19 00:33:50 +0800

    Update hooks.tsx

--- src/state/user/hooks.tsx
+++ src/state/user/hooks.tsx
@@ -185,8 +185,8 @@ export function usePairAdder(): (pair: Pair) => void {
  * @param tokenA one of the two tokens
  * @param tokenB the other token
  */
-export function toV2LiquidityToken([tokenA, tokenB]: [Token, Token], chainId: number): Token {
-  if (chainId === 128) {
+export function toV2LiquidityToken([tokenA, tokenB]: [Token, Token], _chainId: number): Token {
+  if (_chainId === 128) {
     return new Token(tokenA.chainId, Pair.getAddress(tokenA, tokenB), 18, 'Cake-LP', 'Pancake LPs')
   }
   return new Token(tokenA.chainId, Pair.getAddress(tokenA, tokenB), 18, 'PIPI-LP', 'PippiSwap LPs')
