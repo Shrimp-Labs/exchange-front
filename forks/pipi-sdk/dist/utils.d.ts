c592d73d72cd7945f7e7bfb890482c1b06a7dab2
pippiadmin <heco.punk0@gmail.com>
2021-08-19 00:46:45 +0800

    fix: update forks

--- forks/pipi-sdk/dist/utils.d.ts
+++ forks/pipi-sdk/dist/utils.d.ts
@@ -5,5 +5,5 @@ export declare function validateAndParseAddress(address: string): string;
 export declare function parseBigintIsh(bigintIsh: BigintIsh): JSBI;
 export declare function sqrt(y: JSBI): JSBI;
 export declare function sortedInsert<T>(items: T[], add: T, maxSize: number, comparator: (a: T, b: T) => number): T | null;
-export declare const getInitCodeHash: (chainId: ChainId) => "0xd805d4c8a7fb3567167020352386905de5d4bd188fe2284675e3ed584653df75" | "0x8523147bd14c2b44dc8294639a79467599661eec2240f76aef4db80d5193d401";
-export declare const getFactoryAddress: (chainId: ChainId) => "0x979efE7cA072b72d6388f415d042951dDF13036e" | "0x43cE21cdceeC70828220DF623b3B183D86eD1DB2";
+export declare const getInitCodeHash: (chainId: ChainId) => "0xd805d4c8a7fb3567167020352386905de5d4bd188fe2284675e3ed584653df75" | "0x39049b80b4bd4fa78c175418c9994a334451144332c03e8b77b994857fc62178";
+export declare const getFactoryAddress: (chainId: ChainId) => "0x979efE7cA072b72d6388f415d042951dDF13036e" | "0x0dDF434108DF168b347428De9C8F595471364A48";
